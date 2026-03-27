12cce1aa5f2edca06c26829a0effcc1081f89fab
Dmitry Matveyev <public@greenfork.me>
2026-03-27 21:41:53 +0300

    Fix bin/ scripts

--- config/datastar.rb
+++ config/datastar.rb
@@ -2,6 +2,11 @@
 require "datastar"
 require "datastar/async_executor"
 
+# Do not silence logs!
+Datastar::AsyncExecutor.prepend(Module.new do
+  def initialize; end
+end)
+
 Datastar.configure do |config|
   config.finalize = lambda do |_view_context, response|
     throw :halt, response.finish
--- pcelka.rb
+++ pcelka.rb
@@ -3,7 +3,13 @@ require "async"
 require "pcelka/async_client"
 require "pcelka/logs_collector"
 
-procfile = ENV.fetch("PCELKA_PROCFILE", "Procfile")
+procfile = Pathname(ENV.fetch("PCELKA_PROCFILE", "Procfile"))
+unless procfile.exist?
+  procfile = Pathname(__FILE__).parent.join("examples/procfile/Procfile")
+  Console.warn "Running an example proc!"
+  Console.warn "Please run in the directory with Procfile or specify it via the PCELKA_PROCFILE environment variable"
+end
+
 logs_mailbox = Async::Queue.new
 PCELKA = Pcelka::AsyncClient.from_procfile procfile
 PCELKA.server.writer.add_generic_writer logs_mailbox
