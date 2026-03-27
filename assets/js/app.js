b1fd96a16ae30e5e7c1e213a8eda81f1cd4ac1a9
Dmitry Matveyev <public@greenfork.me>
2026-03-27 22:21:15 +0300

    Remove search input, browser search works just fine

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,17 +8,9 @@ function clickIdIfPresent(id) {
   elem.click();
 }
 
-function focusSearch(e) {
-  const elem = document.getElementById("search-logs");
-  if (!elem) return;
-  elem.focus();
-  e.preventDefault();
-}
-
 addHotkeyListener(
   ["s", "ы"],
   () => clickIdIfPresent("scroll-to-bottom-checkbox"),
 );
 addHotkeyListener(["u", "г"], () => clickIdIfPresent("nav-up"));
 addHotkeyListener(["d", "в"], () => clickIdIfPresent("nav-down"));
-addHotkeyListener(["/", "."], focusSearch);
