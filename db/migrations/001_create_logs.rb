4419ddb835212dd2be75048360c03e7ea9c4af21
Dmitry Matveyev <public@greenfork.me>
2026-03-27 21:16:59 +0300

    Add time to logs

--- db/migrations/001_create_logs.rb
+++ db/migrations/001_create_logs.rb
@@ -3,8 +3,9 @@ Sequel.migration do
     create_table :logs do
       primary_key :id
       String :app, null: false
-      String :message, null: false
+      String :message, null: false, text: true
       TrueClass :is_error, null: false, default: false
+      DateTime :created_at, null: false, default: Sequel.lit('unixepoch()')
     end
   end
 end
